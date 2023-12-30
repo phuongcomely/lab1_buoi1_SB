46220040030b152abec346a3afc3c0b97d77aca6
phuongcomely <phuongcomely@gmail.com>
2023-12-30 18:52:12 +0700

    Mini project

---
+++ javascript/MiniProject_buoi5_sboot/src/test/java/vn/techmaster/miniproject_buoi5_sboot/MiniProjectBuoi5SbootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.techmaster.miniproject_buoi5_sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectBuoi5SbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
